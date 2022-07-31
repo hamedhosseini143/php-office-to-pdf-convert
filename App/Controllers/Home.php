74a4f4ea7403bb75a62db17ea7738b34aeb6eaec
root <root@ubuntu-g2-small1-ba-1.novalocal>
2022-07-31 09:13:13 +0000

    conver office to pdf

---
+++ App/Controllers/Home.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\View;
+use NcJoes\OfficeConverter\OfficeConverter;
+
+/**
+ * Home controller
+ *
+ * PHP version 7.0
+ */
+class Home extends \Core\Controller
+{
+
+    /**
+     * Show the index page
+     *œœ
+     * @return void
+     */
+    public function indexAction()
+    {
+        $path = getcwd();
+        $file = $path.'/file/test.docx';
+        var_dump($path);
+        $converter = new OfficeConverter($file);
+        $converter->convertTo('output-file.pdf'); //generates pdf file in same directory as test-file.docx
+        $converter->convertTo('output-file.html'); //generates html file in same directory as test-file.docx
+
+        $converter = new OfficeConverter('test-file.docx', 'path-to-outdir');
+        View::renderTemplate('Home/index.html');
+    }
+}
